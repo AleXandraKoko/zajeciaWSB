10b6c26a8d94e1a67fbd902d73105d690545def1
To ja <student-wroclaw@EDU-WSB.local>
2023-01-21 12:12:59 +0100

    kodwzb

---
+++ kod.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+print(os.getcwd())
+os.chdir('C:\\Users\\vdi-student\\Desktop')
+print(os.getcwd())
+os.mkdir('new_folder')
+time.sleep(2)
+os.rename('new_folder', 'new_folder2')
+time.sleep(2)
+os.rmdir('new_folder2')
